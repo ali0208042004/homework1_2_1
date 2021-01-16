2a23df12b49a2ebc0154bf30746aa3af01869313
ali0208042004 <argen_samatov@mail.ru>
2021-01-16 17:03:51 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String nameOfClass = in.next();
+        switch (nameOfClass) {
+            case "Cat":
+                System.out.println(createObject("Cat").print());
+                break;
+            case "Dog":
+                System.out.println(createObject("Dog").print());
+                break;
+            case "Pig":
+                System.out.println(createObject("Pig").print());
+                break;
+        }
+
+
+    }
+
+    public static Animal createObject(String className) {
+
+        if (className.equals(Cat.class.getSimpleName())) {
+            return new Cat("Сфинск ", 1, "" + "радуга", "Черная");
+        } else if (className.equals(Pig.class.getSimpleName())) {
+            return new Pig("английская свиннья", 2, "Плюха", 225);
+        } else if (className.equals(Dog.class.getSimpleName())) {
+            return new Dog("Авчарка", 1, "Мухтар", "атака");
+        } else {
+            return null;
+        }
+    }
+
+}
